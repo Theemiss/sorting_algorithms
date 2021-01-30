6e3a00057149a4af25535eaf98eec5bb791d0b64
Your Name <midinfotn401@gmail.com>
2021-01-30 21:00:09 +0100

    shell sort Knuth sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * shell_sort - shell Sort Algrithme using Knuth sequence
+ * @array: Array to be Sorted
+ * @size:size of the array
+ * Return:Sorted Array (void)
+ */
+void shell_sort(int *array, size_t size)
+{
+	int n = 1, i, j;
+	int tmp;
+
+	if (!array)
+		return;
+	while (n < (int)size / 3)
+		n = n * 3 + 1;
+
+	while (n > 0)
+	{
+		for (i = n; i < (int)size; i++)
+		{
+			tmp = array[i];
+			j = i;
+			while (j > (n - 1) && array[j - n] >= tmp)
+			{
+				array[j] = array[j - n];
+				j -= n;
+			}
+			array[j] = tmp;
+		}
+		n = n / 3;
+		print_array(array, size);
+	}
+}
