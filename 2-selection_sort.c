d7c0b73255edd14762896b79bbedf95a9120b805
Your Name <midinfotn401@gmail.com>
2021-01-30 17:48:50 +0100

    fixed condition

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,13 +9,13 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i;
+	int i;
 	int min, tmp;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < (int)size; i++)
 	{
 		min = locate_min(array, i, size);
-		if (min == -1)
+		if (min != -1)
 		{
 			tmp = array[i];
 			array[i] = array[min];
@@ -37,11 +37,11 @@ void selection_sort(int *array, size_t size)
 int locate_min(int *array, int index, size_t size)
 {
 	int min, idx_min;
-	size_t i;
+	int i;
 
 	min = array[index];
 	idx_min = index;
-	for (i = index; i < size; i++)
+	for (i = index; i < (int)size; i++)
 	{
 		if (array[i] < min)
 		{
