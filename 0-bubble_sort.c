6925f8306d6b0f5070c68fe91c568407aa5c9f24
Your Name <midinfotn401@gmail.com>
2021-01-31 14:15:57 +0100

    styling

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+
 /**
  *bubble_sort - sorts an array of integers in ascending orde
  *@array: the array to be sort
@@ -6,21 +7,22 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-size_t j, i;
-int n;
-if (size < 2)
-return;
-for (i = 0; i < size - 1; i++)
-{
-for (j = 0; j < size - i - 1; j++)
-{
-if (array[j] > array[j + 1])
-{
-n = array[j];
-array[j] = array[j + 1];
-array[j + 1] = n;
-print_array(array, size);
-}
-}
-}
+	size_t j, i;
+	int n;
+
+	if (size < 2)
+		return;
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				n = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = n;
+				print_array(array, size);
+			}
+		}
+	}
 }
