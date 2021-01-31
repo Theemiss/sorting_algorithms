9ed04eaeca402152bc33f1668a4daa534904adde
eya-98 <eya.nani@sesame.com.tn>
2021-01-31 11:24:36 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,9 +14,9 @@ for (j = 0; j < size - i - 1; j++)
 {
 if (array[j] > array[j + 1])
 {
-n = array[j];
-array[j] = array[j + 1];
-array[j + 1] = n;
+n = array[j + 1];
+array[j + 1] = array[j];
+array[j] = n;
 print_array(array, size);
 }
 }
