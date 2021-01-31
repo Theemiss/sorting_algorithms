be02fbdfa8d0a1d577a1a71799b4b73c69185b8b
eya-98 <eya.nani@sesame.com.tn>
2021-01-31 11:17:13 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,10 +6,9 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-unsigned int n, stop, j, i;
+unsigned int n, j, i;
 for (i = 0; i < size - 1; i++)
 {
-stop = 0;
 for (j = 0; j < size - i - 1; j++)
 {
 if (array[j] > array[j + 1])
@@ -17,11 +16,8 @@ if (array[j] > array[j + 1])
 n = array[j];
 array[j] = array[j + 1];
 array[j + 1] = n;
-stop += 1;
 print_array(array, size);
 }
 }
-if (stop == 0)
-break;
 }
 }
