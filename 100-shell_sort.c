99c2a1628146d018ffe63ea0ba0490d3f507d823
Your Name <midinfotn401@gmail.com>
2021-01-31 14:30:04 +0100

    maybe fixed

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -11,11 +11,12 @@ void shell_sort(int *array, size_t size)
 	int n = 1, i, j;
 	int tmp;
 
-	if (!array)
+	if ((int)size <= 2)
+		return;
+	if (!array || !size)
 		return;
 	while (n < (int)size / 3)
 		n = n * 3 + 1;
-
 	while (n > 0)
 	{
 		for (i = n; i < (int)size; i++)
